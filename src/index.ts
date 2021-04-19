111759f6c2c14a0efeb942ef3edec2943810a3f9
SeungWon <20sunrin041@sunrint.hs.kr>
2021-04-19 11:41:42 +0900

    chore: types

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,28 @@ import express from "express"
 import { makeExecutableSchema } from "@graphql-tools/schema"
 import { bodyParserGraphQL } from "body-parser-graphql"
 import { graphqlHTTP } from "express-graphql"
-import { useSofa, OpenAPI } from "sofa-api"
+import { useSofa } from "sofa-api"
 import { readFileSync } from "fs"
 import expressPlayground from "graphql-playground-middleware-express"
 import DB from "config/connectDB"
-
 import resolvers from "resolvers"
 const typeDefs = readFileSync("src/typeDefs.graphql", "utf-8")
 
 const app = express()
+app.use(bodyParserGraphQL())
+
 const start = async () => {
-    app.use(bodyParserGraphQL())
     const db = await DB.get()
     const schema = makeExecutableSchema({
         typeDefs,
         resolvers
     })
-
     app.use(
         useSofa({
             basePath: '/api',
             schema,
-            context() {
-                return {
-                    db
-                }
+            context: {
+                db
             }
         })
     )
